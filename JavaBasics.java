92a276fec0c9e7ef28fa884e6e0d82bde3767ba5
Helio <75308480+Helio-Centrism@users.noreply.github.com>
2023-10-26 10:28:21 +0530

    Add files via upload

---
+++ JavaBasics.java
@@ -0,0 +1,16 @@
+//to write any code in a java we first define a class for the given problem
+//in this class we define a function main as public static void main(strings args[])
+//main function is the first thing the Java compiler finds when it needs to run the java program
+/*this code which is given below 
+    public class JavaBasics {
+        public static void main(String args[]{
+            
+        })
+    }
+is known as boilerplate code because Boilerplate code is the code that is required for every Java program and is not specific to the problem you are trying to solve. It includes defining a class and the main method, which serves as the entry point for your program
+ */
+public class JavaBasics {
+    public static void main(String args[]){
+        
+    }
+}
