c471b010526e78de4877de28d7e69455e051f355
Helio <75308480+Helio-Centrism@users.noreply.github.com>
2023-10-26 10:30:27 +0530

    Add files via upload

---
+++ Output.java
@@ -0,0 +1,11 @@
+//(" ") this box can gives some output or can take some input
+// we can use print and println both are almost sam just println breaks the line
+//we can also use it for next line we can add this anywhere we want a next line
+// so output in java can done using three ways print, println and \n. 
+public class Output {
+    public static void main(String args[]){
+        System.out.println("hello\nnigga");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
